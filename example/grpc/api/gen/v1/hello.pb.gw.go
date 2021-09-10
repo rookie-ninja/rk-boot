a76abfa599ffec1ddf1601d0a1ab7a262f57faa2
dongxuny <dongxuny@gmail.com>
2021-09-11 05:17:59 +0800

    Bump up rk-grpc version to v1.2.4

--- example/grpc/api/gen/v1/hello.pb.gw.go
+++ example/grpc/api/gen/v1/hello.pb.gw.go
@@ -77,7 +77,7 @@ func RegisterGreeterHandlerServer(ctx context.Context, mux *runtime.ServeMux, se
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.v1.Greeter/SayHello")
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/api.v1.Greeter/SayHello", runtime.WithHTTPPathPattern("/api/v1/SayHello"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -139,7 +139,7 @@ func RegisterGreeterHandlerClient(ctx context.Context, mux *runtime.ServeMux, cl
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.v1.Greeter/SayHello")
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/api.v1.Greeter/SayHello", runtime.WithHTTPPathPattern("/api/v1/SayHello"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 require (
 	github.com/rookie-ninja/rk-entry v1.0.3
 	github.com/rookie-ninja/rk-gin v1.2.3
-	github.com/rookie-ninja/rk-grpc v1.2.2
+	github.com/rookie-ninja/rk-grpc v1.2.3
 	github.com/rookie-ninja/rk-prom v1.1.3
 	github.com/stretchr/testify v1.7.0
 )
