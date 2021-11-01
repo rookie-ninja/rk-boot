69ad5aba834931b7fc717343ed4de175b0c6ec4d
dongxuny <dongxuny@gmail.com>
2021-11-02 03:36:09 +0800

    Enrich boot_test.go

--- boot_test.go
+++ boot_test.go
@@ -7,6 +7,7 @@ package rkboot
 
 import (
 	"context"
+	"fmt"
 	"github.com/rookie-ninja/rk-entry/entry"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
@@ -17,7 +18,7 @@ import (
 	"time"
 )
 
-func TestNewBoot_HappyCase(t *testing.T) {
+func TestNewBoot_HappyCase_Gin(t *testing.T) {
 	config := `
 ---
 gin:
@@ -26,7 +27,7 @@ gin:
     enabled: true
 `
 
-	filePath := createFileAtTestTempDir(t, config)
+	filePath := createFileAtTestTempDir(t, "ut-boot-gin.yaml", config)
 
 	boot := NewBoot(WithBootConfigPath(filePath))
 	boot.AddShutdownHookFunc("ut-shutdown", func() {
@@ -43,19 +44,105 @@ gin:
 	assert.Nil(t, boot.GetConfigEntry(""))
 	assert.Nil(t, boot.GetCertEntry(""))
 	assert.NotNil(t, boot.GetGinEntry("ut-gin"))
-	assert.Nil(t, boot.GetGrpcEntry(""))
+	assert.Nil(t, boot.GetEchoEntry("ut-gin"))
+	assert.Nil(t, boot.GetGrpcEntry("ut-gin"))
 	assert.Nil(t, boot.GetPromEntry(""))
 
 	go func() {
-		time.Sleep(3 * time.Second)
-		rkentry.GlobalAppCtx.GetShutdownSig() <- syscall.SIGTERM
+		boot.WaitForShutdownSig(context.TODO())
 	}()
 
-	boot.WaitForShutdownSig(context.TODO())
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.GetShutdownSig() <- syscall.SIGTERM
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.RemoveEntry("ut-gin")
 }
 
-func createFileAtTestTempDir(t *testing.T, content string) string {
-	tempDir := path.Join(t.TempDir(), "ut-boot.yaml")
+func TestNewBoot_HappyCase_Echo(t *testing.T) {
+	config := `
+---
+echo:
+  - name: ut-echo
+    port: 8080
+    enabled: true
+`
+
+	filePath := createFileAtTestTempDir(t, "ut-boot-echo.yaml", config)
+
+	boot := NewBoot(WithBootConfigPath(filePath))
+	boot.AddShutdownHookFunc("ut-shutdown", func() {
+		// noop
+	})
+
+	boot.Bootstrap(context.TODO())
+
+	assert.NotNil(t, boot.GetAppInfoEntry())
+	assert.NotNil(t, boot.GetZapLoggerEntry(rkentry.DefaultZapLoggerEntryName))
+	assert.NotNil(t, boot.GetZapLoggerEntryDefault())
+	assert.NotNil(t, boot.GetEventLoggerEntry(rkentry.DefaultEventLoggerEntryName))
+	assert.NotNil(t, boot.GetEventLoggerEntryDefault())
+	assert.Nil(t, boot.GetConfigEntry(""))
+	assert.Nil(t, boot.GetCertEntry(""))
+	assert.Nil(t, boot.GetGinEntry("ut-echo"))
+	assert.NotNil(t, boot.GetEchoEntry("ut-echo"))
+	assert.Nil(t, boot.GetGrpcEntry("ut-echo"))
+	assert.Nil(t, boot.GetPromEntry(""))
+
+	go func() {
+		boot.WaitForShutdownSig(context.TODO())
+	}()
+
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.GetShutdownSig() <- syscall.SIGTERM
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.RemoveEntry("ut-echo")
+}
+
+func TestNewBoot_HappyCase_Grpc(t *testing.T) {
+	config := `
+---
+grpc:
+  - name: ut-grpc
+    port: 8080
+    enabled: true
+`
+
+	filePath := createFileAtTestTempDir(t, "ut-boot-grpc.yaml", config)
+
+	boot := NewBoot(WithBootConfigPath(filePath))
+	fmt.Println(filePath)
+	boot.AddShutdownHookFunc("ut-shutdown", func() {
+		// noop
+	})
+
+	boot.Bootstrap(context.TODO())
+
+	assert.NotNil(t, boot.GetAppInfoEntry())
+	assert.NotNil(t, boot.GetZapLoggerEntry(rkentry.DefaultZapLoggerEntryName))
+	assert.NotNil(t, boot.GetZapLoggerEntryDefault())
+	assert.NotNil(t, boot.GetEventLoggerEntry(rkentry.DefaultEventLoggerEntryName))
+	assert.NotNil(t, boot.GetEventLoggerEntryDefault())
+	assert.Nil(t, boot.GetConfigEntry(""))
+	assert.Nil(t, boot.GetCertEntry(""))
+	assert.Nil(t, boot.GetGinEntry("ut-grpc"))
+	assert.Nil(t, boot.GetEchoEntry("ut-grpc"))
+	assert.NotNil(t, boot.GetGrpcEntry("ut-grpc"))
+	assert.Nil(t, boot.GetPromEntry(""))
+
+	go func() {
+		boot.WaitForShutdownSig(context.TODO())
+	}()
+
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.GetShutdownSig() <- syscall.SIGTERM
+	time.Sleep(1 * time.Second)
+	rkentry.GlobalAppCtx.RemoveEntry("ut-grpc")
+
+}
+
+func createFileAtTestTempDir(t *testing.T, filename, content string) string {
+	tempDir := path.Join(t.TempDir(), filename)
+
 	assert.Nil(t, ioutil.WriteFile(tempDir, []byte(content), os.ModePerm))
 	return tempDir
 }
